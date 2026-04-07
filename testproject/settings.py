33a7d46ba5d86f1464a1a651590be022c9ce1ee0
BALASURIYA-PERIYASAMY <balasuriyaperiyasamy@gmail.com>
2026-04-07 19:22:34 +0530

    created the Django

--- testproject/settings.py
+++ testproject/settings.py
@@ -75,7 +75,11 @@ WSGI_APPLICATION = 'testproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'testproject',
+        'USER': 'postgres',
+        'PASSWORD': '6422',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
