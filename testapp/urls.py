83988df66047396307e460875927dee8cd81bcf4
BALASURIYA-PERIYASAMY <balasuriyaperiyasamy@gmail.com>
2026-04-16 19:19:21 +0530

    updated codes

---
+++ testapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import CreateTestView, LoginView, QuestionView, RegisterView, UploadTestCSVView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('create-test/', CreateTestView.as_view(), name='create-test'),
+    path('list-tests/', CreateTestView.as_view(), name='list-tests'),
+    path('create-questions/', QuestionView.as_view(), name='create-questions'),
+    path('bulk-upload/', UploadTestCSVView.as_view(), name='bulk-upload'),
+]
